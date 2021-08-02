ea7326e1fbad457024177cdd2f1a7446aa9f2756
P0L-L33 <aron.kovacs543@gmail.com>
2021-08-02 15:45:14 +0300

    Added Spring Security. Added users as secure endpoint.
    Added login authentication/response/etc.

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplementation.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplementation.java
@@ -1,7 +1,12 @@
 package com.appsdeveloperblog.app.ws.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -46,6 +51,15 @@ public class UserServiceImplementation implements UserService {
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
 		
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UserEntity userEntity = userRepository.findByEmail(email);
+		
+		if(userEntity == null) throw new UsernameNotFoundException(email);
+		
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
 
 }
